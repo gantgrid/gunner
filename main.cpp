1bde9fdf25fcffa47155e0b32180da4abc3cdecf
gantgrid <gantgrid@gmail.com>
2012-11-26 05:38:36 -0800

    first commit
    Mon Nov 26 05:39:18 PST 2012
    First Day github noob

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+ Mon Nov 26 04:28:28 PST 2012
+*/
+#include <iostream>
+using namespace std;
+  
+int main(int argc, char *argv[])
+{
+  cout<<"Your Name is?\n";
+  char name[20];
+  cin>>name;
+  cout<<" \n nice "<<name<<
+    " it worked\n";
+  cout<<"Shutting myself down now\n";
+  return 0;
+}
